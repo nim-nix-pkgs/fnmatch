9fb2ca39177b19bb5bc252cd7b660593b3758dfb
hugosenari <hugosenari@gmail.com>
2022-05-09 20:37:22 -0300

    chore: re index fnmatch

--- flake.nix
+++ flake.nix
@@ -7,14 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."fnmatch-master".dir   = "master";
-  inputs."fnmatch-master".owner = "nim-nix-pkgs";
-  inputs."fnmatch-master".ref   = "master";
-  inputs."fnmatch-master".repo  = "fnmatch";
-  inputs."fnmatch-master".type  = "github";
-  inputs."fnmatch-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."fnmatch-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
